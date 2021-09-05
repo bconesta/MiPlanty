be368e2cad726bcc71400603f69774e90583479b
Bruno Conesta <bconesta@etrr.edu.ar>
2021-09-04 23:34:37 -0300

    Login page started

--- miplanty/src/app/app.module.ts
+++ miplanty/src/app/app.module.ts
@@ -7,10 +7,31 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNiyFcye6U5eOv2aaNRe_tAlJgYbvfAVI",
+  authDomain: "mi-planty.firebaseapp.com",
+  databaseURL: "https://mi-planty-default-rtdb.firebaseio.com",
+  projectId: "mi-planty",
+  storageBucket: "mi-planty.appspot.com",
+  messagingSenderId: "1092620526838",
+  appId: "1:1092620526838:web:d9bfa1cd4bc92b1221b8d7"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule    
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
