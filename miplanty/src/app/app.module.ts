b768763187ca0914e01406ed6397ec9eff8c331f
Antonella De Cia <adecia@etrr.edu.ar>
2021-09-06 16:49:12 -0300

    6-9 error server

--- miplanty/src/app/app.module.ts
+++ miplanty/src/app/app.module.ts
@@ -24,7 +24,7 @@ const firebaseConfig = {
 };
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BluetoothSerial],
   entryComponents: [],
   imports: [
     BrowserModule,
