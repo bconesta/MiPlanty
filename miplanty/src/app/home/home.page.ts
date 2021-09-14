04852a7af24e9b1c7bcc2c1956c353adcc93ac93
Bruno Conesta <bconesta@etrr.edu.ar>
2021-09-14 13:33:49 -0300

    Logout function enabled

--- miplanty/src/app/home/home.page.ts
+++ miplanty/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../servicios/auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-home',
@@ -6,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
-
+  constructor(private authService : AuthService, public router : Router) { }
+  back(){
+    this.authService.logout().then(res =>{
+      this.router.navigate(['/login']);
+    }).catch(err => alert('Los datos son incorrectos'))
+    
+  }
+  
   ngOnInit() {
   }
 
