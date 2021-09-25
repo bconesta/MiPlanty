b9c04e03a38f1e0f74d3df7da1482cdf092a0764
Bruno Conesta <bconesta@etrr.edu.ar>
2021-09-24 22:05:37 -0300

    UNCON case finished

--- MI/src/main.cpp
+++ MI/src/main.cpp
@@ -27,7 +27,7 @@ String inst = "-";
 int address = 0;
 int lecturat, lecturah, lectural = 0;
 float voltaje, grados = 0;
-bool btEn, instChanged, SSID_select, PASS_select = false;
+bool btEn, instChanged, SSID_select, PASS_select, SSID_changed, PASS_changed = false;
 
 void setup() {
   Serial.begin(115200);
@@ -68,16 +68,42 @@ void loop() {
       //EN CASO DE QUE EL WIFI NO ESTE CONECTADO
       if(!btEn){SerialBT.begin("MiPlanty"); btEn=true;}
       
-      if(SerialBT.available()){
-        inst = SerialBT.readString();
-        instChanged = true;
+      if(SerialBT.available()){           //Si hay datos disponibles
+        inst = SerialBT.readString();     //se leen y se almacenan en la variable "inst"
+        instChanged = true;               //y se da aviso que inst fue modificada, con la variable "instChanged" (si inst fue modificada, 
+      }                                   //es lo mismo que decir que se leyeron datos nuevos del puerto serie)
+      
+      if(inst == "SSID" && instChanged) SSID_select=true; //Si el dato recibido fue "SSID", eso indica que el siguiente dato recibido será el SSID de la red
+      else if(inst == "PASS" && instChanged) PASS_select=true; //Si el dato recibido fue "PASS", eso indica que el siguiente dato recibido será la password de la red
+      else if(SSID_select && instChanged){SSID = inst; SSID_select = false; SSID_changed = true;} //El dato recibido es el SSID de la red
+      else if(PASS_select && instChanged){PASS = inst; PASS_select = false; PASS_changed = true;} //El dato recibido es la password de la red
+      
+      if(SSID_changed && PASS_changed){
+        //CONVERSIÓN DE STRING A ARRAY DE CHAR PARA USAR EN METODO begin() de WiFi
+        char SSID_c[SSID.length()];
+        for (int i = 0; i < sizeof(SSID); i++) {
+            SSID_c[i] = SSID[i];
+        }
+        char PASS_c[PASS.length()];
+        for (int i = 0; i < sizeof(PASS); i++) {
+            PASS_c[i] = PASS[i];
+        }
+        //FIN DE CONVERSIÓN 
+        WiFi.begin(SSID_c, PASS_c);
+        SerialBT.print("Intentando");
+        SSID_changed = false;
+        PASS_changed = false;
       }
 
-      if(inst == "SSID" && instChanged == true) SSID_select=true;
-      else if(inst == "PASS" && instChanged == true) PASS_select=true;
-
-
       instChanged = false;
+
+      //CONDICION CAMBIO DE ESTADO
+      if(WiFi.status() == WL_CONNECTED){
+        SerialBT.print("Conectado");
+        estado=CON_FIREUNCON;
+        SerialBT.end();
+        btEn = false;
+      }
     break;
     
     case CON_FIREUNCON:
