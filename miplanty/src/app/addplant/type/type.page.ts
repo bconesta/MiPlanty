7336e02bd7091cc434d2abe6af2784be25de2540
Bruno Conesta <bconesta@etrr.edu.ar>
2021-09-26 02:09:24 -0300

    Avances con añadir planta

--- miplanty/src/app/addplant/type/type.page.ts
+++ miplanty/src/app/addplant/type/type.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { OptionsPage } from '../options/options.page';
 @Component({
   selector: 'app-type',
   templateUrl: './type.page.html',
@@ -7,8 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypePage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router, public modalController: ModalController) { }
+  back(){
+    this.router.navigate(['/home']);
+  }
+  
+  next(){
+    this.router.navigate(['/add/connection']);
+  }
 
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: OptionsPage,
+      cssClass: 'my-custom-class'
+    });
+    return await modal.present();
+  }
+  
   ngOnInit() {
   }
 
